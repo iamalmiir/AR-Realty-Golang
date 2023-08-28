93e6fec8a76140ed071d700763ea76cdac5e2ef0
Almir R. <r9@almir.io>
2023-08-27 20:38:33 -0400

    code update

---
+++ models/user.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id" gorm:"type:char(36);primary_key;default:uuid()"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
